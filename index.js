c807e132c6488be75140a7f52d38f6ff8b5a7628
mandersen21 <mikkelandersen21@gmail.com>
2018-08-05 13:05:37 +0200

    Added changes.

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ app.listen(port, () => console.log('Listeling on port ' + port))
 
 app.get('/name', callName);
 
-function callName(req, res) {
+// function callName(req, res) {
      
-    // Use child_process.spawn method from 
-    // child_process module and assign it
-    // to variable spawn
-    var spawn = require("child_process").spawn;
+//     // Use child_process.spawn method from 
+//     // child_process module and assign it
+//     // to variable spawn
+//     var spawn = require("child_process").spawn;
      
-    // Parameters passed in spawn -
-    // 1. type_of_script
-    // 2. list containing Path of the script
-    //    and arguments for the script 
+//     // Parameters passed in spawn -
+//     // 1. type_of_script
+//     // 2. list containing Path of the script
+//     //    and arguments for the script 
      
-    // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
-    // so, first name = Mike and last name = Will
-    var process = spawn('python',["./python.py",
-                            req.query.firstname,
-                            req.query.lastname] );
+//     // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+//     // so, first name = Mike and last name = Will
+//     var process = spawn('python',["./python.py",
+//                             "Mikkel",
+//                             "Mikkel2"] );
  
-    // Takes stdout data from script which executed
-    // with arguments and send this data to res object
-    process.stdout.on('data', function(data) {
+//     // Takes stdout data from script which executed
+//     // with arguments and send this data to res object
+//     process.stdout.on('data', function(data) {
 
-        console.log("Data from script: ", data.toString())
+//         console.log("Data from script: ", data.toString())
 
-        res.send(data.toString());
-    } )
-}
+//         res.send(data.toString());
+//     } )
+// }
