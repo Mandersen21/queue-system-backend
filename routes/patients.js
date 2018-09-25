974c1968939cf1453ec8f4187a6b8e9493e14aa2
mandersen21 <mikkelandersen21@gmail.com>
2018-09-25 15:01:58 +0200

    Added config, added patient service with methods.

--- routes/patients.js
+++ routes/patients.js
@@ -1,101 +1,15 @@
 const express = require('express');     // Express
 const Joi = require('joi');             // Joi for validating models 
-const moment = require('moment')        // Moment for calculating waiting times
+const moment = require('moment');       // Moment for calculating waiting times
+
+// Service
+const service = require('../services/patientService');
 
 const router = express.Router();
-
-const triage = {
-    IMMEDIATE: 1,
-    VERY_URGENT: 2,
-    URGENT: 3,
-    STANDARD: 4,
-    NON_URGENT: 5
-}
+const patients = service.getMockPatients();
 
 router.get('/', (req, res) => {
     res.send(patients)
 });
 
-function getUniqeID() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-};
-
-// TODO - test this works as intended
-function getPatientInitials(fullname) {
-    let firstnameCharacter = fullname.split(" ")[0];
-    let lastnameCharacter = fullname.split(" ")[1];
-    return (firstnameCharacter.charAt(0) + lastnameCharacter.charAt(0)).toUpperCase();
-};
-
-// TODO - add logic to retrieve estimated waiting time, note use Moment libery
-function getWaitingTime(registredTime) {
-    return new Date()
-};
-
-// Mock data
-const patients = [
-    {
-        id: getUniqeID(),
-        fullname: 'Anders Andersen',
-        patientInitials: getPatientInitials('Anders Andersen'),
-        triage: triage.STANDARD,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Mikkel Andersen',
-        patientInitials: getPatientInitials('Mikkel Andersen'),
-        triage: triage.STANDARD,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Mads Wehlast',
-        patientInitials: getPatientInitials('Anders Andersen'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Patrick Rasmussen',
-        patientInitials: getPatientInitials('Patrick Rasmussen'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Søren Spætte',
-        patientInitials: getPatientInitials('Søren Spætte'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Lisbeth Thy',
-        patientInitials: getPatientInitials('Lisbeth Thy'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Claus Kluder',
-        patientInitials: getPatientInitials('Claus Kluder'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-    {
-        id: getUniqeID(),
-        fullname: 'Nicolaj Bjarkesen',
-        patientInitials: getPatientInitials('Nicolaj Bjarkesen'),
-        triage: triage.NON_URGENT,
-        registredTime: new Date(),
-        waitingTime: getWaitingTime(registredTime),
-    },
-];
+module.exports = router;
---
+++ services/patientService.js
@@ -0,0 +1,99 @@
+module.exports = {
+
+    getUniqeID: function () {
+        return '_' + Math.random().toString(36).substr(2, 18);
+    },
+
+    getPatientInitials: function (fullname) { // TODO - test this works as intended when user enters a middle name
+        let firstnameCharacter = fullname.split(" ")[0];
+        let lastnameCharacter = fullname.split(" ")[1];
+        return (firstnameCharacter.charAt(0) + lastnameCharacter.charAt(0)).toUpperCase();
+    },
+
+    getPatients: function () { // TODO - Add database and retreive from it to get a list of current patients in the queue
+
+    },
+
+    getWaitingTime: function () { // TODO - add logic to retrieve estimated waiting time, note use Moment libery
+        return new Date()
+    },
+
+    getMockPatients: function () {
+        return [
+            {
+                id: this.getUniqeID(),
+                fullname: 'Anders Andersen',
+                patientInitials: this.getPatientInitials('Anders Andersen'),
+                triage: triage.STANDARD,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Mikkel Andersen',
+                patientInitials: this.getPatientInitials('Mikkel Andersen'),
+                triage: triage.STANDARD,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Mads Wehlast',
+                patientInitials: this.getPatientInitials('Anders Andersen'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Patrick Rasmussen',
+                patientInitials: this.getPatientInitials('Patrick Rasmussen'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Søren Spætte',
+                patientInitials: this.getPatientInitials('Søren Spætte'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Lisbeth Thy',
+                patientInitials: this.getPatientInitials('Lisbeth Thy'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Claus Kluder',
+                patientInitials: this.getPatientInitials('Claus Kluder'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+            {
+                id: this.getUniqeID(),
+                fullname: 'Nicolaj Bjarkesen',
+                patientInitials: this.getPatientInitials('Nicolaj Bjarkesen'),
+                triage: triage.NON_URGENT,
+                registredTime: new Date(),
+                waitingTime: this.getWaitingTime(new Date()),
+            },
+        ];
+    }
+}
+
+// Triage enum
+const triage = {
+    IMMEDIATE: 1,
+    VERY_URGENT: 2,
+    URGENT: 3,
+    STANDARD: 4,
+    NON_URGENT: 5
+}
+
