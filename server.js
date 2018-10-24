3fed9dec0dcbe59967413973eee6f3ae99dc078e
mandersen21 <mikkelandersen21@gmail.com>
2018-10-25 00:13:25 +0200

    Added pusher changes.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express');
 const config = require('config');
 const helmet = require('helmet');
@@ -7,31 +9,34 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Routes
+const bodyParser = require("body-parser");
+
+// ------------------------------
+// Set routes
+// ------------------------------
 const patients = require('./routes/patients')
 
-// Database connection
+// ------------------------------
+// Set up DB connection
+// ------------------------------
 mongoose.connect('mongodb://localhost/waitingtimes')
     .then(() => console.log("Connected to waitingtimes database"))
     .catch(err => console.log("Could not connect to the database: waitingtimes", err))
 
-// App settings
+// ------------------------------
+// Create express app
+// ------------------------------
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(helmet());
 app.use('/api/patients', patients);
 
 // Start server
 app.listen(port, () => console.log('Listening on port ' + port))
-
-
-
-
-
-
-
 
 // function callName(req, res) {
 
