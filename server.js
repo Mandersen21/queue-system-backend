1b9f3406531e8fe4262e7421e39d0c4cb13ba327
mandersen21 <mikkelandersen21@gmail.com>
2018-11-17 15:14:58 +0100

    Added boduparser back in.

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
 const express = require('express');
 const winston = require('winston');
 const cors = require('cors');
 const app = express();
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/prod')(app);
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static('public'));
-// app.use(helmet());
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info('Listening on port ' + port))
